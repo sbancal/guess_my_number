4d75e4c265e7d8e7349572bba1ca2b30832caa1f
Samuel Bancal <Samuel.Bancal@epfl.ch>
2020-10-02 15:59:37 +0200

    [ADD](GuessMachine)→ Class

--- guess_my_number.py
+++ guess_my_number.py
@@ -4,6 +4,11 @@
 Created on Fri Sep 25 16:13:09 2020
 
 @author: Samuel Bancal
+
+This offers a Class to play at "Guess my number".
+
+It can also be run as is ... which will play interactively
+with the user and let him look for the mystery number.
 """
 
 import random
@@ -11,20 +16,52 @@ import random
 MIN = 0
 MAX = 1000
 
+
+class GuessMachine():
+    '''
+    I have a number in mind,
+    you have to guess it!!
+
+    + self._number_to_guess is generated durinig creation of the object
+    + Use `guess(num)` method to make a guess
+    + I'll count the number ot attempt in self.number_of_attempt
+    '''
+
+    def __init__(self):
+
+        self._number_to_guess = random.randint(MIN, MAX)
+        self.number_of_attempt = 0
+
+    def guess(self, num):
+        '''
+        Attempt to find the right number
+        it returns 'too high', 'too low', 'found'
+        '''
+        self.number_of_attempt += 1
+        if num < self._number_to_guess:
+            return 'too low'
+        elif num > self._number_to_guess:
+            return 'too high'
+        else:
+            return 'found'
+
+
 if __name__ == '__main__':
-    number_to_guess = random.randint(MIN, MAX)
+    guess_nachine = GuessMachine()
     print('Hey! Try to guess a number between %d and %d!' % (MIN, MAX))
 
     while True:
         user_input = input('Your guess? ')
         try:
             user_attempt = int(user_input)
-            if user_attempt == number_to_guess:
-                print('Fantastic, you coud find the number I had in mind!')
+            result = guess_nachine.guess(user_attempt)
+            if result == 'found':
+                print(
+                    'Fantastic, you coud find '
+                    'the number I had in mind '
+                    'in %d attempts!' % guess_nachine.number_of_attempt)
                 break
-            elif user_attempt < number_to_guess:
-                print('too low')
             else:
-                print('too high')
+                print(result)
         except ValueError:
             print('You joker ... that was not an integer!')
