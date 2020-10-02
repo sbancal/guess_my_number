4572be55c12188ecb78c70cbaa3a56894d295159
Samuel Bancal <Samuel.Bancal@epfl.ch>
2020-10-02 16:24:51 +0200

    [UPG](computer_player.py)→ Level 1 Computer player
    It's strategy is to try random numbers between min and max.
    It'll adapt min and max values to match GuessMachine answers.

--- computer_player.py
+++ computer_player.py
@@ -8,7 +8,8 @@ Created on 2020-10-02
 This script will try to guess the mysterious number
 prepared by guess_my_number.GuessMachine.
 
-It's strategy is to try random numbers.
+It's strategy is to try random numbers between min and max.
+It'll adapt min and max values to match GuessMachine answers.
 """
 
 import random
@@ -16,11 +17,17 @@ import random
 from guess_my_number import MIN, MAX, GuessMachine
 
 if __name__ == '__main__':
+    min = MIN
+    max = MAX
     guess_nachine = GuessMachine()
     while True:
-        attempt = random.randint(MIN, MAX)
+        attempt = random.randint(min, max)
         result = guess_nachine.guess(attempt)
         print('tried %d : %s' % (attempt, result))
         if result == 'found':
             print('Finished in %d attempts.' % guess_nachine.number_of_attempt)
             break
+        elif result == 'too low':
+            min = attempt + 1
+        else:
+            max = attempt - 1
